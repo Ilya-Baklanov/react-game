5438fcc46db21fd8d2ddec3683e0b8a6237f0cb9
Ilya_Baklanov <60309676+Ilya-Baklanov@users.noreply.github.com>
2021-03-07 19:44:10 +0400

    refactor: class component to functional component

--- src/components/NewGame/NewGame.tsx
+++ src/components/NewGame/NewGame.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
 
-import { State, Props } from './NewGame.model';
+import { Props } from './NewGame.model';
 import styles from './style.scss';
 
-class NewGame extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    const { newGame } = this.props;
-    return (
-      <React.Fragment>
-        <button
-          type="button"
-          className={styles['new-game-button']}
-          onClick={newGame}
-        >
-          New Game
-        </button>
-      </React.Fragment>
-    );
-  }
-}
+const NewGame = ({ newGame }: Props): JSX.Element => (
+  <React.Fragment>
+    <button
+      type="button"
+      className={styles['new-game-button']}
+      onClick={newGame}
+    >
+      New Game
+    </button>
+  </React.Fragment>
+);
 
 export default NewGame;
